1753ff6bbe21f9bfa004385f29d365c32e38857d
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2024-03-28 21:19:52 +0300

    Add MongoDB connection and update dependencies

--- server/Auth/passport.js
+++ server/Auth/passport.js
@@ -1,28 +1,8 @@
-const connection = require("../db/dbConfig");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const User = require("./UserModel");
 
 module.exports = function (passport) {
-  passport.serializeUser((user, done) => {
-    done(null, user.userId);
-  });
-
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const [rows, fields] = await connection.execute(
-        "SELECT * FROM Users WHERE userId = ?",
-        [id]
-      );
-      if (rows.length === 0) {
-        return done(new Error("User not found"));
-      }
-      const user = rows[0];
-      done(null, user);
-    } catch (error) {
-      console.error("Error deserializing user:", error);
-      done(error);
-    }
-  });
-
   passport.use(
     new GoogleStrategy(
       {
@@ -31,49 +11,35 @@ module.exports = function (passport) {
         callbackURL: "/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
-        const { id, displayName, givenName, familyName, emails } = profile;
-        const userId = id;
+        const newUser = {
+          googleId: profile.id,
+          displayName: profile.displayName,
+          firstName: profile.name.givenName,
+          lastName: profile.name.familyName,
+          image: profile.photos[0].value,
+        };
 
         try {
-          // Check if the user already exists in the database
-          const [rows, fields] = await connection.execute(
-            "SELECT * FROM Users WHERE userId = ?",
-            [userId]
-          );
-          if (rows.length > 0) {
-            // User already exists, return the user
-            const user = rows[0];
-            return done(null, user);
+          let user = await User.findOne({ googleId: profile.id });
+
+          if (user) {
+            done(null, user);
+          } else {
+            user = await User.create(newUser);
+            done(null, user);
           }
-
-          // If the user doesn't exist, create a new user record
-          const username = displayName;
-          const firstName = givenName;
-          const lastName = familyName;
-          const email = emails[0].value;
-
-          // Perform database query to save user data
-          const [result, _] = await connection.execute(
-            "INSERT INTO Users (username, firstName, lastName, email) VALUES (?, ?, ?, ?)",
-            [username, firstName, lastName, email]
-          );
-
-          // Create a user object without password
-          const newUser = {
-            userId: result.insertId,
-            username,
-            firstName,
-            lastName,
-            email,
-          };
-
-          // Return the newly created user
-          done(null, newUser);
-        } catch (error) {
-          console.error("Error authenticating user:", error);
-          done(error);
+        } catch (err) {
+          console.error(err);
         }
       }
     )
   );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => done(err, user));
+  });
 };
